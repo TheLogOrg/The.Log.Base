c306d42d0d0f47608f261ada033d17a339ad516f
Wolf Bublitz <wolf.bublitz@gmail.com>
2022-07-16 19:41:51 +0200

    updated Check-Version

--- ci/Check-Version.ps1
+++ ci/Check-Version.ps1
@@ -8,11 +8,13 @@ $versionElement = Select-Xml -Path $projectFilePath -XPath "/Project/PropertyGro
 $version = $versionElement.Node.InnerXml
 
 if ($version.Equals($tagName)) {
+    Write-Host "[SUCC] " -ForegroundColor Green -NoNewline
     Write-Host "Version from project matches tag name!"
 
     exit 0
 } else {
-    Write-Error "Version mismatch"
+    Write-Host "[FAIL] " -ForegroundColor Red -NoNewline
+    Write-Host "Version mismatch"
     Write-Host "Expected: $tagName"
     Write-Host "Actual: $projectVersion"
     exit 1
